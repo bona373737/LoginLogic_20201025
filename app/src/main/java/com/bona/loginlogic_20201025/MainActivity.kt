9d20039f37951d5bf726ed4d165328241b065bc0
bona <bona373737@gmail.com>
2020-10-25 13:06:26 +0900

    kt단에 로그인 if로직 작성

---
+++ app/src/main/java/com/bona/loginlogic_20201025/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.bona.loginlogic_20201025
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+            val inputEmail = emailEdt.text.toString()
+            val inputPw = passwordEdt.text.toString()
+
+            if (inputEmail == "admin@test.com" && inputPw == "qwer"){
+                Toast.makeText(this,"관리자가 로그인했습니다.", Toast.LENGTH_SHORT)show()
+            }
+            else{Toast.makeText(this, "로그인실패",Toast.LENGTH_SHORT)show()}
+
+        }
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
